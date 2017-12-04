1832acbb76f275414a7c53b13e1eff46ec15cb51
Freddy Persson <FreddyPersson85@gmail.com>
2017-12-04 11:04:10 +0100

    Test of auto-git

--- app.js
+++ app.js
@@ -2,52 +2,44 @@ const express = require('express');
 const scraperjs = require('scraperjs');
 
 const app = express();
+let weatherTD;
+let weatherInformation = [];
 
-let newsFromAB = [];
-let newsFromDN = [];
+async function scrapeWeather() {
+	let weatherSite = scraperjs.StaticScraper.create('http://www.dmi.dk/vejr/til-lands/byvejr/by/vis/SE/2662149/Ystad,%20Sverige');
+	let columnText = ['time', 'degrees', 'rain', 'wind'];
+	let informationToPush = {};
 
-async function scrapeAftonbladet() {
-	let ab = scraperjs.StaticScraper.create('https://aftonbladet.se');
-
-	let news = await ab.scrape(function($) {
-	    return $('a[href*="/nyheter"]').map(function() {
-	        return {
-	        	text: $(this).closest('h3').text(),
-	        	url: 'https://aftonbladet.se' + $(this).attr('href');
-	    }).get();
+	weatherTD = await weatherSite.scrape(function($) {
+		return $($('#hour_table table')[0]).find('tbody td').filter(function(index) {
+			if (index % 5 === 1) {
+				return false;
+			}
+		});
 	});
-	newsFromAB = news;
+
+	for (let i = 0; i < weatherTD.length; i++) {
+
+		let column = i % 4;
+
+		weatherInformation[] = columnText[column];
+
+		if (i === 0 && column === 0) {
+			weatherInformation.push(informationToPush);
+			informationToPush = {};
+		}
+	}
+
+	console.log(weatherInformation);
 }
 
-async function scrapeDN() {
-	let ab = scraperjs.StaticScraper.create('https://dn.se');
+scrapeWeather();
+setInterval(scrapeWeather, 60 * 1000);
 
-	let news = await ab.scrape(function($) {
-	    return $('a[href*="/nyheter"]').map(function() {
-	        return {
-	        	text: $(this).closest('h3').text(),
-	        	url: 'https://dn.se' + $(this).attr('href');
-	    }).get();
-	});
-	newsFromDN = news;
-}
-
-scrapeAftonbladet();
-setInterval(scrapeAftonbladet, 60 * 1000);
-
-scrapeDN();
-setInterval(scrapeDN, 60 * 1000);
-
-app.get('/ab-news', (req, res) => {
-	res.json(newsFromAB);
-});
-
-app.get('/dn-news', (req, res) => {
-	res.json(newsFromDN);
-});
-
-app.get('/all-news', (req, res) => {
-	res.json(newsFromAB + newsFromDN);
+app.get('/weather', (req, res) => {
+	res.json('weatherInformation');
 });
 
 app.listen(3000,() => console.log("Listening on port 3000."));
+
+//bam
